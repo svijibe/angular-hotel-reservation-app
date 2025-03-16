c0de0f274bdfac307c3735a611f8b334d2244ded
VijiSubra <viji.subramanyan@yahoo.com>
2025-03-16 07:27:37 -0400

    Creating Reservarion Form

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ReservationFormComponent {
 
+  reservationForm: FormGroup = new FormGroup({});
+
+  onSubmit(){
+    
+  }
 }
