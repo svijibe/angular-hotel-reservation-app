4fc6f89da52bf93271b670e738845b3504c1d661
VijiSubra <viji.subramanyan@yahoo.com>
2025-03-16 17:38:41 -0400

    deleting reservation

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -18,6 +18,8 @@ export class ReservationListComponent {
     this.reservations = this.reservationService.getReservations();
   }
 
-  
+  deleteReservation(id: string){
+    this.reservationService.deleteReservation(id);
+  }
 
 } 
